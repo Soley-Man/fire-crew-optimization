2004654b91973275676bc780059e0bde6fc666f7
Soley-Man <59509876+Soley-Man@users.noreply.github.com>
2025-05-12 17:47:40 -0400

    Implement cost function

--- sa-fire-crew-optimization.py
+++ sa-fire-crew-optimization.py
@@ -108,6 +108,94 @@ def count_overlapping_unavailabilities(ranger_ids: list[int], n=0):
 
     return count
 
+def avg_experience(crew: list[int]) -> float:
+    '''
+    Return the average experience of crew.
+    '''
+    return round( stat.mean([int(fire_rangers[ranger_id]["Years of Experience"]) for ranger_id in crew]), 2)
+
+def personal_prefs_penalty(crew: list[int]) -> int:
+    '''
+    Return the cost of unsatisfied personal preferences within crew.
+    '''
+    # Count rangers with unsatisfied 'same crew' personal preferences
+    unsatisfied_rangers = 0
+
+    for ranger_id in crew:
+        if fire_rangers[ranger_id]["Same crew preferences"]:
+            # If any of the preferences are satisfied, the Ranger is satisfied
+            if not any([is_in_crew(crew, name) for name in fire_rangers[ranger_id]["Same crew preferences"]]):
+                unsatisfied_rangers += 1
+    
+    # Count unsatisfied 'different crew' preferences
+    diff_crew_violations = 0
+
+    for ranger_id in crew:
+        for name in fire_rangers[ranger_id]["Different crew preferences"]:
+            if is_in_crew(crew, name):
+                diff_crew_violations += 1
+    
+    # Return cost increase
+    return (unsatisfied_rangers + diff_crew_violations) * 10
+
+def understaffing_penalty(crew: list[int], leadership_indexes: list[int]) -> int:
+    '''
+    Return the cost of understaffing in crew.
+
+    Precondition: len(leadership_indexes) = 2
+    '''
+    cost = 0
+
+    # Check for leadership understaffing (at least one Crew Leader or Boss must be present in a crew)
+    days_understaffed_leadership = count_overlapping_unavailabilities(leadership_indexes)
+
+    cost += days_understaffed_leadership * 500
+
+    # Check for general understaffing (at least 3 Fire Rangers must be present in a crew)
+    if len(crew) == 4:  # 4 person crew
+        # At most 1 Ranger can be absent from the crew
+        days_understaffed = count_overlapping_unavailabilities(crew, n = 1)
+
+        cost += days_understaffed * 100
+
+    elif len(crew) == 5:  # 5 person crew
+        # At most 2 rangers may be absent from the crew
+        days_understaffed_by_1 = count_overlapping_unavailabilities(crew, n = 1)
+        days_understaffed_by_2 = count_overlapping_unavailabilities(crew, n = 2)
+        
+        # The cost increases by 100 per day understaffed, per ranger missing
+        cost += days_understaffed_by_1 * 100 + days_understaffed_by_2 * 2 * 100
+    
+    return cost
+
+def mixed_crew_restrictions_penalty(crew: list[int]) -> int:
+    '''
+    Return the cost of violations of mixed crew restrictions within crew.
+    '''
+    restrictions_violated = 0
+
+    if is_mixed_gender(crew):
+        for ranger_id in crew:
+            if has_mixed_gender_restriction(ranger_id):
+                restrictions_violated += 1
+    
+    return 500 * restrictions_violated
+
+def fitness_certification_penalty(crew: list[int], leadership_indexes: list[int]) -> int:
+    '''
+    Return the cost of unsatisfied fitness certification requirements.
+
+    Precondition: len(leadership_indexes) = 2
+    '''
+    # Check if at least one member of Crew leadership is certified to National standards
+    if not any([is_certified(idx) for idx in leadership_indexes]):
+        return 100
+    
+    # Check if less than 4 Rangers are certified to National standards
+    amount_certified = len([ranger_id for ranger_id in crew if is_certified(ranger_id)]) 
+    if amount_certified < 4:
+        return (4 - amount_certified) * 50
+
 # Data Preparation
 ## Read Fire Rangers data
 fire_rangers = csv_to_dict('fire-rangers-data.csv')
@@ -156,7 +244,7 @@ for ranger in fire_rangers:
         pass
 
 ## Calculate average experience across the fire base
-avg_experience_base = round( stat.mean([int(ranger["Years of Experience"]) for ranger in fire_rangers]), 2)
+avg_base_experience = round( stat.mean([int(ranger["Years of Experience"]) for ranger in fire_rangers]), 2)
 
 # Initialize Solution
 crew_ids = [n for n in range(1, crews_n + 1)] * 4
@@ -183,9 +271,32 @@ def calculate_cost(solution: list[int]) -> int:
     '''
     cost = 0
     crew_assignment = rangers_to_crew(solution)
+    diff_crew_base_xp = []
 
     for crew_id, ranger_ids in crew_assignment.items():
-        pass
+        leadership_idx = [idx for idx in ranger_ids if idx in leaders_idx + bosses_idx]
+
+        # Personal Preferences Penalty
+        cost += personal_prefs_penalty(ranger_ids)
+
+        # Understaffing Penalty
+        cost += understaffing_penalty(ranger_ids, leadership_idx)
+
+        # Mixed Crew Restriction Penalty
+        cost += mixed_crew_restrictions_penalty(ranger_ids)
+
+        # Fitness Certification Penalty
+        cost += fitness_certification_penalty(ranger_ids, leadership_idx)
+
+        # Record squared difference between average crew experience and average base experience
+        avg_crew_exp = avg_experience(ranger_ids)
+        diff_crew_base_xp.append((avg_crew_exp - avg_base_experience) ** 2)
+
+    # Experience Variance Penalty
+    experience_variance = sum(diff_crew_base_xp) / len(crew_assignment)
+    cost += experience_variance * 100
+
+    return cost
 
 # Create Neighbour
 
